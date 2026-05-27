bcfdf227215a7e4e0b6c35ed463ecd12503ae939
Rihosepp-40B <rihosepp@gmail.com>
2026-05-27 22:14:26 +0300

    add TaskManagerContext

---
+++ FullStack_CRUD_APP.Server/Data/TaskManagerContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FullStack_CRUD_APP.Server.Data
+{
+	public class TaskManagerContext : DbContext
+	{
+		public TaskManagerContext(DbContextOptions<TaskManagerContext> option) : base(option) // constructor
+		{ }
+
+		public DbSet<Tasks> Tasks { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Tasks>().ToTable("TaskManager");
+		}
+	}
+}
